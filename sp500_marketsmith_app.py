fb3bb460129018e12099b5b4b0abdca408b6300b
dhusho19 <hushon.d@googlemail.com>
2022-07-18 20:54:03 +0100

    Sector Summary & Sector Ranking Average added

--- sp500_marketsmith_app.py
+++ sp500_marketsmith_app.py
@@ -81,18 +81,25 @@ def app():
         # use the newly created weight column to calculate sector rank
         df_selected_sector['Sector Rank'] = df_selected_sector['weight']*df_selected_sector['Ind Group Rank']
         df_selected_sector['Sector Rank'] = df_selected_sector['Sector Rank'].astype('float32').round(2).astype('int')
-
+        # take the average of the weighted values by date by sector
+        df_selected_sector['Sector Rank Avg'] = df_selected_sector.groupby([df_selected_sector.index,'Sector'])['Sector Rank'].transform('mean')
+        # the sum of the sector rank by date by sector
         df_sector_rank = df_selected_sector.groupby([df_selected_sector.index,'Sector'])['Sector Rank'].sum().reset_index()
+        df_sector_rank_avg = df_selected_sector.groupby([df_selected_sector.index,'Sector'])['Sector Rank Avg'].mean().reset_index()
+        # df formatting & merge
         df_sector_rank.set_index('Date',inplace=True)
+        df_sector_final = pd.merge(df_sector_rank, df_sector_rank_avg, how='left', on=['Date', 'Sector'])
 
         st.markdown("""---""")
         # function calls
-        sector_crossover_strategy(df_sector_rank)
+        sector_crossover_strategy(df_sector_final)
         industry_crossover_strategy(df_selected_industry)
-        plotting(df_sector_rank,df_selected_industry,selected_sector,selected_industry)
+        plotting(df_sector_final,df_selected_industry,selected_sector,selected_industry)
         st.markdown("""---""")
+        df_sector_daily_changes = summary_sector(df)
         df_daily_changes = summary(df)
         st.markdown("""---""")
+        daily_sector_signal_changes(df_sector_daily_changes)
         daily_signal_changes(df_daily_changes)
     else:
         st.subheader("About")
@@ -103,15 +110,15 @@ def app():
 def sector_crossover_strategy(df):
     if sma_ema == 'SMA':
         # Sector
-        df[short_term_col] = df['Sector Rank'].rolling(window=short_term, min_periods=1).mean()
-        df[mid_term_col] = df['Sector Rank'].rolling(window=mid_term, min_periods=1).mean()
-        df[long_term_col] = df['Sector Rank'].rolling(window=long_term, min_periods=1).mean()
+        df[short_term_col] = df['Sector Rank Avg'].rolling(window=short_term, min_periods=1).mean()
+        df[mid_term_col] = df['Sector Rank Avg'].rolling(window=mid_term, min_periods=1).mean()
+        df[long_term_col] = df['Sector Rank Avg'].rolling(window=long_term, min_periods=1).mean()
 
     elif sma_ema == 'EMA':
         # Sector
-        df[short_term_col] = df['Sector Rank'].ewm(span=short_term, adjust=False).mean()
-        df[mid_term_col] = df['Sector Rank'].ewm(span=mid_term, adjust=False).mean()
-        df[long_term_col] =  df['Sector Rank'].ewm(span=long_term, adjust=False).mean()
+        df[short_term_col] = df['Sector Rank Avg'].ewm(span=short_term, adjust=False).mean()
+        df[mid_term_col] = df['Sector Rank Avg'].ewm(span=mid_term, adjust=False).mean()
+        df[long_term_col] =  df['Sector Rank Avg'].ewm(span=long_term, adjust=False).mean()
 
     # signal alerts for crossover strategy Sector
     df['alert_st'] = 0.0
@@ -144,6 +151,7 @@ def industry_crossover_strategy(df):
     df['position_st'] = df['alert_st'].diff()
     df['position_lt'] = df['alert_lt'].diff()
 
+
 def plotting(df_sector_rank, df_selected_industry,selected_sector,selected_industry):
 
     st.header('Graphing Sectors & IG')
@@ -151,9 +159,9 @@ def plotting(df_sector_rank, df_selected_industry,selected_sector,selected_indus
     if st.checkbox('Plot Sector Ranking Graph'):
         st.subheader('IBD Sector Ranking')
 
-        fig = px.line(df_sector_rank, x=df_sector_rank.index, y=['Sector Rank',df_sector_rank[short_term_col],df_sector_rank[mid_term_col],df_sector_rank[long_term_col]],
+        fig = px.line(df_sector_rank, x=df_sector_rank.index, y=['Sector Rank Avg',df_sector_rank[short_term_col],df_sector_rank[mid_term_col],df_sector_rank[long_term_col]],
                         hover_name='Sector', template = 'plotly_dark',
-                        color_discrete_map={'Sector Rank':'white', short_term_col:'green',mid_term_col:'yellow',long_term_col:'red'}
+                        color_discrete_map={'Sector Rank Avg':'white', short_term_col:'green',mid_term_col:'yellow',long_term_col:'red'}
                          )
 
         fig.add_scatter(x=df_sector_rank.loc[df_sector_rank['position_st'] == -1].index,
@@ -190,7 +198,7 @@ def plotting(df_sector_rank, df_selected_industry,selected_sector,selected_indus
                         legend_title ='')
 
         fig.update_yaxes(autorange="reversed")
-        fig.update_traces(patch={"line": {"dash": 'dot'}}, selector={"legendgroup": "Sector Rank"})
+        fig.update_traces(patch={"line": {"dash": 'dot'}}, selector={"legendgroup": "Sector Rank Avg"})
 
         # checkbox to hide and show the buy & sell dataframe
         if st.checkbox('Buy & Sell Sector Data'):
@@ -203,14 +211,14 @@ def plotting(df_sector_rank, df_selected_industry,selected_sector,selected_indus
             sorted_sector_df.reset_index(inplace=True)
 
             # call download function, with a subset of the data. Only looking at rows for buy and sell triggers
-            csv = convert_df(sorted_sector_df.loc[:,['Date','Sector','Sector Rank',short_term_col,long_term_col,'Buy Sell ST']].loc[(sorted_sector_df['position_st'].isin([-1,1]))])
+            csv = convert_df(sorted_sector_df.loc[:,['Date','Sector','Sector Rank Avg',short_term_col,long_term_col,'Buy Sell ST']].loc[(sorted_sector_df['position_st'].isin([-1,1]))])
             st.download_button(label="Download data as CSV",
                                data=csv,
                                file_name='Sector_Latest_Signals.csv',
                                mime='text/csv')
 
             # write df to streamlit app
-            st.write(sorted_sector_df.loc[:,['Date','Sector','Sector Rank','Buy Sell ST']].loc[(sorted_sector_df['position_st'].isin([-1,1]))].head(3))
+            st.write(sorted_sector_df.loc[:,['Date','Sector','Sector Rank Avg','Buy Sell ST']].loc[(sorted_sector_df['position_st'].isin([-1,1]))].head(3))
 
         return st.plotly_chart(fig)
 
@@ -281,25 +289,103 @@ def plotting(df_sector_rank, df_selected_industry,selected_sector,selected_indus
                                file_name='IG_Latest_Signals.csv',
                                mime='text/csv')
             # write df to streamlit app
-            st.write(sorted_industry_df.loc[:,['Date','Sector','Name','Ind Group Rank',short_term_col,mid_term_col,long_term_col,'position_st','position_lt','Buy Sell ST','Buy Sell LT']].loc[(sorted_industry_df['position_st'].isin([-1,1]))].head(3))
+            st.write(sorted_industry_df.loc[:,['Date','Sector','Name','Ind Group Rank','Buy Sell ST']].loc[(sorted_industry_df['position_st'].isin([-1,1]))].head(3))
 
         return st.plotly_chart(fig)
 
 
+def summary_sector(df):
+    st.header('Sector & IG Summary')
+    if st.checkbox('Sector'):
+        df.reset_index(inplace=True)
+
+        # Create a IG list of all unqiue IG's
+        sector_lst = sorted(df['Sector'].unique().tolist())
+
+        # Iterate through each IG and load into a list & convert to an NumPy array
+        lst = []
+        for i in sector_lst:
+            df_sector = df.loc[(df['Sector'] == i)]
+            sector_ranking(df_sector)
+            sector_crossover_strategy(df_sector)
+            lst.append(df_sector)
+
+        # create an empty list to store all dataframes, then concatenate them at the end of the iteration
+        dfs = []
+        for x in lst:
+            dfs.append(x)
+        df1_sector = pd.concat(dfs, ignore_index=True)
+
+        # create buy and sell column, to easily identify the triggers
+        df1_sector['Buy Sell ST'] = np.where(df1_sector['alert_st'] == 0,'BUY','SELL')
+        df1_sector['Buy Sell LT'] = np.where(df1_sector['alert_lt'] == 0,'BUY','SELL')
+
+        # Filter Dataframes to only look at rows which are signals
+        df_st = df1_sector.loc[df1_sector['position_st'].isin([-1,1])]
+        df_lt = df1_sector.loc[df1_sector['position_lt'].isin([-1,1])]
+        frames = [df_st, df_lt]
+        df_sector_final = pd.concat(frames)
+        df_sector_final.sort_values(by=['Date'], ascending=True, inplace=True)
+        #st.write(df_sector_final)
+
+        # Pull back the latest signal per Sector
+        df_sector_final = df_sector_final.groupby('Sector').tail(1).reset_index(drop=True)
+        df_sector_final.drop(['total_mkt_val','weight','Sector Rank','alert_st','alert_lt','position_st','position_lt'], axis=1, inplace=True)
+
+        # Rounding formatting
+        df_sector_final[short_term_col] = df_sector_final[short_term_col].astype('float32').round(2).astype('int')
+        df_sector_final[mid_term_col] = df_sector_final[mid_term_col].astype('float32').round(2).astype('int')
+        df_sector_final[long_term_col] = df_sector_final[long_term_col].astype('float32').round(2).astype('int')
+        df_sector_final['Sector Rank Avg'] = df_sector_final[long_term_col].astype('float32').round(2).astype('int')
+
+        # Sort DataFrame and reshape it to merge each IG onto the one row
+        df_sector_final.sort_values(by=['Sector','Date'], ascending=True, inplace=True)
+
+        # Re order the column structure
+        unique_sectors = sorted(df_sector_final['Sector'].unique().tolist())
+        sector_options = st.multiselect('Sectors of Interest',unique_sectors, default=unique_sectors, key="1")
+        # Find the latest Ind Group Rank / Mkt Val and pull it  through to the summary
+        max_date = df['Date'].max()
+        df_latest = df.loc[df['Sector'].isin(sector_options) & (df['Date'] == max_date)]
+
+        df_sector_final = df_sector_final.loc[df_sector_final['Sector'].isin(sector_options)]
+        df_sector_final2 = pd.merge(df_sector_final, df_latest, on=['Symbol','Name','Sector'], how='left')
+        # Rename the columns were two instances occur, validation the data is correct pulling through date
+        df_sector_final2.rename(columns = {'Date_x':'Date', 'Date_y':'Latest Date','Ind Group Rank_y':'Ind Group Rank', 'Ind Mkt Val (bil)_y':'Ind Mkt Val (bil)'},inplace=True)
+        # Drop the latest date column & dropped the instances of IG Rnk / Mkt Val when the signal occurred.
+        df_sector_final2.drop(['Ind Group Rank_x','Ind Mkt Val (bil)_x', 'Latest Date'], axis=1, inplace=True)
+        # Re-order the dataframe
+        df_sector_final2 = df_sector_final2.reindex(columns=['Date','Symbol','Name','Sector','Sector Rank Avg','Ind Mkt Val (bil)',short_term_col,mid_term_col,long_term_col,'Buy Sell ST','Buy Sell LT'])
+        st.write(df_sector_final2)
+
+        # Call download function
+        csv = convert_df(df_sector_final2)
+        st.download_button(label="Download data as CSV",
+            data=csv,
+            file_name='Sector_Latest_Signals.csv',
+            mime='text/csv')
+
+        return df_sector_final2
+
+
 def summary(df):
-    st.header('IG Summary')
+    #st.header('IG Summary')
     if st.checkbox('IG'):
         df.reset_index(inplace=True)
+        # an index column appears if you select the sector's summary first, which affects the reshaping. Therefore, need to drop the column if it exists.
+        if 'index' in df.columns:
+            df.drop('index', axis=1, inplace=True)
+
         # Create a IG list of all unqiue IG's
         industry_lst = sorted(df['Name'].unique().tolist())
 
         # Iterate through each IG and load into a list & convert to an NumPy array
-        lst = []
-        for i in industry_lst:
-            df_industry = df.loc[(df['Name'] == i)]
+        ig_lst = []
+        for ig in industry_lst:
+            df_industry = df.loc[(df['Name'] == ig)]
             industry_crossover_strategy(df_industry)
-            lst.append(df_industry)
-        arr = np.asarray(lst)
+            ig_lst.append(df_industry)
+        arr = np.asarray(ig_lst)
 
         # Load the array which is storing the data into a DataFrame
         df1 = pd.DataFrame(arr.reshape(-1, 13), columns=['Date','Symbol','Name','Sector','Ind Group Rank','Ind Mkt Val (bil)',short_term_col,mid_term_col,long_term_col,'alert_st','alert_lt','position_st','position_lt'])
@@ -330,13 +416,8 @@ def summary(df):
 
         # Re order the column structure
         unique_sectors = sorted(df_final['Sector'].unique().tolist())
-        sector_options = st.multiselect('Sectors of Interest',unique_sectors, default=unique_sectors) #['AEROSPACE','AGRICULTRE','ALCOHL/TOB','AUTO'
-                                                                                      #,'BANKS','CHIPS','CONSUMER'
-                                                                                      #,'ENERGY','FOOD/BEV'
-                                                                                      #,'INSURANCE','INTERNET','SOFTWARE'
-                                                                                      #,'MEDICAL','METALS','MINING','MISC'
-                                                                                      #,'TELECOM','TRANSPRT'
-                                                                                      #,'RETAIL','REAL EST','UTILITY'])
+        sector_options = st.multiselect('Sectors of Interest',unique_sectors, default=unique_sectors,key="2")
+
         # Find the latest Ind Group Rank / Mkt Val and pull it  through to the summary
         max_date = df['Date'].max()
         df_latest = df.loc[df['Sector'].isin(sector_options) & (df['Date'] == max_date)]
@@ -361,11 +442,43 @@ def summary(df):
         return df_final2
 
 
+def sector_ranking(df):
+    # create a total market value to utilise for weighted sector rank, then add this to the dataframe
+    total_mkt_val = df.groupby(['Date','Sector'])['Ind Mkt Val (bil)'].transform('sum')
+    df['total_mkt_val'] = total_mkt_val
+
+    # calculate a percentage weight for each industry
+    df['weight'] = df['Ind Mkt Val (bil)']/df['total_mkt_val']
+    # use the newly created weight column to calculate sector rank
+    df['Sector Rank'] = df['weight']*df['Ind Group Rank']
+    df['Sector Rank'] = df['Sector Rank'].astype('float32').round(2).astype('int')
+    df['Sector Rank Avg'] = df.groupby(['Date','Sector'])['Sector Rank'].transform('mean')
+    df_sector_rank = df.groupby(['Date','Sector'])['Sector Rank'].sum().reset_index()
+    df_sector_rank_avg = df.groupby(['Date','Sector'])['Sector Rank Avg'].mean().reset_index()
+
+    df_sector_rank.set_index('Date',inplace=True)
+    df_sector_final = pd.merge(df_sector_rank, df_sector_rank_avg, how='left', on=['Date', 'Sector'])
+
+    return df_sector_final
+
+
+def daily_sector_signal_changes(df):
+    """
+    Look max date in dataframe which is filter on signals only, then compare this when them IG previous signal.
+    """
+    st.header('Sector & IG Daily Changes')
+    if st.checkbox('Sector Signal Changes'):
+        df['Date'] = pd.to_datetime(df['Date']).dt.date
+        max_date = df['Date'].max()
+        df_sector_daily = df.loc[(df['Date'] == max_date)]
+        st.write(df_sector_daily)
+
+
 def daily_signal_changes(df):
     """
     Look max date in dataframe which is filter on signals only, then compare this when them IG previous signal.
     """
-    st.header('IG Daily Changes')
+    #st.header('IG Daily Changes')
     if st.checkbox('IG Signal Changes'):
         df['Date'] = pd.to_datetime(df['Date']).dt.date
         max_date = df['Date'].max()
